53cdc43aadd2ea823beadaff51b4f098c94daf5f
Nishant Kumar <nk534031@gmail.com>
2022-11-23 00:46:03 +0530

    Adding Mutiply Filr

---
+++ multiply.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cout << "Enter the two number : ";
+    cin >> n >> m;
+    int mul = n * m;
+    cout << "Mutiplication of Two Number: " << mul << endl;
+    return 0;
+}
