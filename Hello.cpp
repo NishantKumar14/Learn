97a9ab20e2f449164e3163175acbcaed8c6fc3e1
Nishant Kumar <nk534031@gmail.com>
2022-11-23 00:32:51 +0530

    initial commit

---
+++ Hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cout << "Enter the value of a and b : ";
+    cin >> a >> b;
+    int sum = a + b;
+    cout << "Sum of Two number : " << sum << endl;
+    return 0;
+}
